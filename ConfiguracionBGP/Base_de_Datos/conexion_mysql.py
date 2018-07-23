f4e0faed03162c630d87e399f0c41ac9889f4392
EdPaulSanchez <rhdmdz1@gmail.com>
2018-07-22 21:59:05 -0500

    Update conexion_mysql.py

--- ConfiguracionBGP/Base_de_Datos/conexion_mysql.py
+++ ConfiguracionBGP/Base_de_Datos/conexion_mysql.py
@@ -153,12 +153,39 @@ def all_users(conexion):
 
     #llenar las listas paralelas con la informacion de cada usuario
     for id_u,user,pwd in admins+monitoreo:
-        usuarios.append(u)
+        usuarios.append(user)
         contraseñas.append(pwd)
         ids_usuarios.append(id_u)
 
     #retornar las listas paralelas en una tupla
     return ids_usuarios,usuarios,contraseñas
+
+
+#=============================================================================================
+# all_empresas: hace una consulta a la BD, y retorna una tupla de listas paralelas con las id,nombre y ASNs de las empresas
+# 
+#Parametros:
+# conexion   (objeto):  conexion realizada a la base de datos
+#
+# Retorna: [ids_empresas],[nombres],[ASNs]  --- listas paralelas con las id,usuarios y contraseñas
+#--------------------------------------------------------------------------------------------
+def all_empresas(conexion):
+    #listas con todos las empresas    
+    empresas=consultar_empresas(conexion) #[(idEmpresa, nombre, ASN)]
+    
+    #creacion de listas paralelas
+    nombres=[]
+    ASNs=[]
+    ids_empresas=[]
+
+    #llenar las listas paralelas con la informacion de cada empresa
+    for id_e,nombre,asn in empresas:
+        nombres.append(nombre)
+        ASNs.append(asn)
+        ids_empresas.append(id_e)
+
+    #retornar las listas paralelas en una tupla
+    return ids_empresas,nombres,ASNs
 
 #=============================================================================================
 # establecer_sesion: genera una variable de sesion con el usuario y contraseña ingresados
@@ -170,6 +197,7 @@ def all_users(conexion):
 #
 # Retorna: {'id':<id_usuario>,'username': <usuario>} -- diccionario
 #--------------------------------------------------------------------------------------------
+
 def establecer_sesion(conexion,usuario,contraseña):
     
     id_usuarios,usuarios,contraseñas=all_users(conexion) #crea las listas con la informacion de los usuarios
@@ -180,27 +208,79 @@ def establecer_sesion(conexion,usuario,contraseña):
     return {'id':id_usuarios[search],'username': usuarios[search]}
 
 #=============================================================================================
-#consultar_usuarios: realiza la consulta de todos los usuarios segun el rol
-#siempre y cuando la conexion sea exitosa
+# consultar_devices_empresa: realiza la consulta de todos los dispositivos donde existe el usuario y en una empresa especifica
+# siempre y cuando la conexion sea exitosa
 #Parametros:
 # conexion   (objeto):  conexion realizadaa a la base de datos
 # sesion   (diccionario): variable de sesion con las claves id y user
-# id_empresa  (string): id de la empresa a consultar donde esta el usuario
+# id_empresa  (string): id de la empresa a consultar los dispositivos donde esta el usuario
 #
 # Retorna: lista_dispositivos -- lista de tuplas con la idDispositivo, nombre, gateway del dispositivo
-#                           [idDispositivo, nombre, gateway]
+#                           [(idDispositivo, nombre, gateway)]
 #--------------------------------------------------------------------------------------------
 
-def consultar_dispositivos_usuarios_empresa(conexion,sesion,id_empresa):
+def consultar_devices_empresa(conexion,sesion,id_empresa):
     
     cursor=conexion.cursor()   #crear cursor
+    
     #Query donde se seleccionan los dispositivos de la empresa y a los que pertenece el usuario
-    query="SELECT idDispositivo,nombre,gateway FROM dispositivos INNER JOIN usuarios_dispositivos WHERE idEmpresa='{}' and usuario_dispositivo.idDispositivo=idDispositivo and usuario_dispositivo.idUsuario='{}'".format(id_empresa,sesion['id'])
+    query="SELECT dispositivos.idDispositivo,dispositivos.nombre,dispositivos.gateway FROM dispositivos INNER JOIN usuario_dispositivo WHERE dispositivos.empresa='{}' and usuario_dispositivo.idDispositivo=dispositivos.idDispositivo and usuario_dispositivo.idUsuario='{}' and dispositivos.estado='activo'".format(id_empresa,sesion['id'])
     cursor.execute(query)
+    
     lista_dispositivos=list(cursor.fetchall())
+    
     cursor.close()  #cerrar cursor
     conexion.commit()
+
+    return lista_dispositivos
+
+#=============================================================================================
+# consultar_interfaces: realiza la consulta de todas las interfaces de un dispositivo
+# siempre y cuando la conexion sea exitosa
+#Parametros:
+# conexion   (objeto):  conexion realizadaa a la base de datos
+# id_empresa  (string): id del dispositivo
+#
+# Retorna: lista_interfaces -- lista de tuplas con el nombre, ipAddress, mascara del dispositivo
+#                           [(nombre, ipAddress, mascara)]
+#--------------------------------------------------------------------------------------------
+def consultar_interfaces(conexion,id_dispositivo):
+    cursor=conexion.cursor()   #crear cursor
     
+    #Query donde se seleccionan todas las interfaces de un dispositivo dado
+    query="SELECT nombre,ipAddress,mascara FROM interfaces WHERE idDispositivo='{}'".format(id_dispositivo)
+    cursor.execute(query)
+    
+    lista_interfaces=list(cursor.fetchall())
+    
+    cursor.close()  #cerrar cursor
+    conexion.commit()
+
+    return lista_interfaces
+
+#=============================================================================================
+# consultar_empresas: realiza la consulta de todas las empresas
+# siempre y cuando la conexion sea exitosa
+#Parametros:
+# conexion   (objeto):  conexion realizadaa a la base de datos
+#
+# Retorna: lista_empresas -- lista de tuplas con la idEmpresa, nombre,ASN
+#                           [(idEmpresa, nombre, ASN)]
+#--------------------------------------------------------------------------------------------
+def consultar_empresas(conexion):
+    cursor=conexion.cursor()   #crear cursor
+    
+    #Query donde se seleccionan todas las empresas existentes
+    query="SELECT idEmpresa,nombre,ASN FROM empresa"
+    cursor.execute(query)
+    
+    lista_empresas=list(cursor.fetchall())
+    
+    cursor.close()  #cerrar cursor
+    conexion.commit()
+
+    return lista_empresas
+
 #===================================================================
 # cerrar_conexion: realiza el cierre de la conexion de MySQL
 # Parametros:
@@ -211,8 +291,3 @@ def cerrar_conexion(conexion):
     conexion.close()
     
 
-
-    
-
-
-    
